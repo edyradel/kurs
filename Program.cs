5d2f43ea7c993c6cfc5d88ac05d608f73e1439f4
edyradel <radelska.edyta@gmail.com>
2021-06-11 08:51:13 +0200

    test1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+ 
+
+namespace kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine("Hi Pawel");
+            for(int i=0;i<10;i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(rand.Next(100,999));
+            }
+        }
+    }
+}
